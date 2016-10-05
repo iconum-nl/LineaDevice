79eb34b9fa9b518664c996cabfb7104151c0f32c
BartBakker82 <bartbakker82@gmail.com>
2016-10-05 15:38:37 +0200

    Fixed error on line 311: paseInt -> parseInt

--- www/ScannerLib-Linea-2.0.0.js
+++ www/ScannerLib-Linea-2.0.0.js
@@ -308,7 +308,7 @@ ScannerDevice = function (mappings){
 	*/
 	this.getCharging = function (callback){
 		this.sendCommand('getCharging', [], function (params){
-			callback(Boolean(paseInt(params[0])));
+			callback(Boolean(parseInt(params[0])));
 		});
 	};
 	/**
